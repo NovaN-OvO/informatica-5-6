1e73f8220d7f1e3b8386d3de5e68a05252c8f1f7
Wave_Funct1on <gato.punteado@gmail.com>
2026-08-13 20:20:25 +0000

    Functions and Variables intro

--- hello.py
+++ hello.py
@@ -1,7 +1,23 @@
 
 
 def main():
-    print("Hello, world!")
+    planet =input("Planet: ")
+    #Names of variables must be simple and use "_"
+
+    # Separation
+    print("Hello", planet)
+    # The comma "," is equal to a space in the parenthesis
+
+    #Ending
+    print("Hello", end=" ")
+    print(planet)
+
+    #Concatenation
+    print("Hello " + planet)
+
+    # Formatted String
+    print(f"Hello {planet}")
+    #Ill be using this one!
 
 if __name__ == "__main__":
     main()
