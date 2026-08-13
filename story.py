4e208a1ef138d1a3e3cc3ab878af717c0814861a
Wave_Funct1on <gato.punteado@gmail.com>
2026-08-13 17:21:01 +0000

    story intro

---
+++ story.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    planet =input("Planet: ")
+    #Names of variables must be simple and use "_"
+
+    # Separation
+    print("Hello", planet)
+    # The comma "," is equal to a space in the parenthesis
+
+    #Ending
+    print("Hello", end=" ")
+    print(planet)
+
+    #Concatenation
+    print("Hello " + planet)
+
+    # Formatted String
+    print(f"Hello {planet}")
+
+if __name__ == "__main__":
+    main()
