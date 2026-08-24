043a61baf217055e384a8d8fff543d86a2fafbe9
Wave_Funct1on <gato.punteado@gmail.com>
2026-08-24 19:39:00 +0000

    P4ssw0rd

--- password.py
+++ password.py
@@ -2,6 +2,7 @@ def main():
 
     password = "w4v3_funct10n"
     attempt = input("Enter the password: ")
+    # Use "getpass" search in youtube or ask ChatGPT
 
     if attempt == password:
         print("Correct Password!")
