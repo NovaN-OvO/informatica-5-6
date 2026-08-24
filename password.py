e05a61b47b80bf85d7b15020209db691e0c23632
Wave_Funct1on <gato.punteado@gmail.com>
2026-08-24 19:38:06 +0000

    Password

---
+++ password.py
@@ -0,0 +1,12 @@
+def main():
+
+    password = "w4v3_funct10n"
+    attempt = input("Enter the password: ")
+
+    if attempt == password:
+        print("Correct Password!")
+
+    print()
+
+if __name__ == "__main__":
+    main()
