26d504cd29d6ef2e6206005bd0d5fa853e1870e7
Wave_Funct1on <gato.punteado@gmail.com>
2026-09-01 16:39:14 +0000

    Coin

---
+++ coinflip.py
@@ -0,0 +1,23 @@
+import random
+
+def main():
+    guess = input("What face of the coin do you predict you will get: ").strip().lower()
+    coin = random.randint(1,2)
+
+    if coin == 1:
+        coin = "heads"
+    elif coin == 2:
+        coin = "tails"
+
+    print(f"Coin: {coin}") #print("coin: ", coin)
+
+    if guess == coin:
+        print("You won!")
+    elif guess != coin:
+        print("You lost.")
+    else:
+        print("Invalid option")
+
+
+if __name__ == "__main__":
+    main()
