73703206b7381db6b531f50e6beb8140a7a5d3e9
Wave_Funct1on <gato.punteado@gmail.com>
2026-09-08 23:39:06 +0000

    LP END

--- life-loop.py
+++ life-loop.py
@@ -1,19 +1,33 @@
 import time
 def main():
 
-    dur = time.time()
-    start = -300
+    dur1 = time.time()
     begin = ""
     print("Welcome to the bellwork timer.")
     print("You will have 5 minutes to complete the bellwork")
-    print("Multiply your remaining seconds by 18.5 to get your score! (5k points max.)")
-    begin = input("Press enter to start.")
+    begin = input("Press enter to finish the timer. ")
     if begin == "":
 
-        while start < 0:
-            time.sleep(1)
-            start += 1
-            print(start)
+        dur2 = time.time()
+        ttime = dur2 - dur1
+        ttime = round(ttime, 0)
+        print(f"It took you {ttime}s to finish the bellwork.")
+
+        if ttime <= 300:
+            points = (300 - ttime) * 18.5
+            points = round(points, 0)
+            print(f"Your score is: {points}")
+            if points >= 5000:
+                print("Impossible... You're a robot!")
+            elif points > 3000:
+                print("Good job!")
+            elif points > 1500:
+                print("Keep going!")
+            else:
+                print("Keep working!")
+
+        else:
+            print("Time's up!")
 
 
 
