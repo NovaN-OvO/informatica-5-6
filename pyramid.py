84acf38c70146009cb78b8ec3f04259f02751064
Wave_Funct1on <gato.punteado@gmail.com>
2026-09-17 20:21:20 +0000

    Challenge

--- pyramid.py
+++ pyramid.py
@@ -1,7 +1,8 @@
 def main():
     pyramid = int(input("Enter the height of the pyramid: "))
     asterisco = "*"
+    espacio = " "
     for h in range(pyramid):
-        print(asterisco * (h+1))
+        print((espacio * (pyramid - (h+1))) + (asterisco * (h+1)) + (asterisco * (h+1)))
 if __name__ == "__main__":
     main()
